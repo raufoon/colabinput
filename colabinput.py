f798f4d692e07e4b9afbf465a0157acd06bcf747
raufoon <128422316+raufoon@users.noreply.github.com>
2024-08-10 09:36:51 +0600

    Fixed bugs and spacing

--- colabinput.py
+++ colabinput.py
@@ -5,8 +5,9 @@ import re
 import time
 
 _varnamefriendly = lambda x: re.sub(r'[^a-zA-Z0-9_]', '', x)
-
+_n = 0
 def coinput(**elements):
+    global _n
     display(Javascript('''// Check if a button with id "EnterBtn" already exists
         var existingButton = document.getElementById("EnterBtn");
         if (existingButton) {
@@ -31,20 +32,22 @@ def coinput(**elements):
 
     for i, (name, element) in enumerate(elements.items()):
         #print(i, element, name)
-        element["id"] = f"{element['type']}_{_varnamefriendly(element['label'])}_{i+1}"
+        _n += 1
         element["name"] = name if "name" not in element else element["name"]
+        element["id"] = f"{name}_{element['type']}_{_varnamefriendly(element['label'])}_{_n}"
         if not (element.get("step") is None):
             element["step"] = str(element["step"])
     def getHTML(element):
       match element["type"]:
         case "text" | "password" | "email" | "url" | "number" | "date" | "time" | "datetime-local" | "month" | "week" | "color" | "search" | "file" :
-            return f'''<label for="{element['id']}">{element["label"]}</label>
-                        <input id="{element['id']}" type="{element['type']}" name="{element['name']}" value="{element.get('value', '')}" step="{element.get('step','')}" placeholder="{element.get('placeholder','')}" style = "min-width: {max(20,len(str(element.get('value', ''))))}ch; font-family: monospace" oninput="this.style.width = ((this.value.length>=20) ? (this.value.length) : 20)+'ch';"/>'''
+            return f'''<label for="{element['id']}" style="margin-top:5px;" >{element["label"]}</label>
+                        <input id="{element['id']}" type="{element['type']}" name="{element['name']}" value="{element.get('value', '')}" step="{element.get('step','')}" placeholder="{element.get('placeholder','')}" style = "min-width: {max(20,len(str(element.get('value', ''))))}ch; font-family: monospace; padding:3px; margin:5px; border-radius: 3px;" oninput="this.style.width = ((this.value.length>=20) ? (this.value.length) : 20)+'ch';"/>'''
         case "range":
-            return f'''<label for="{element['id']}">{element["label"]}</label>
+            return f'''<label for="{element['id']}" style="margin-top:5px;" >{element["label"]}</label>
                         <input id="{element['id']}" type="range" name="{element['name']}"
                                value="{element.get('value', '')}" min="{element.get('min', 0)}" max="{element.get('max', 100)}" 
                                step="{element['step']}"
+                               style="padding:3px; margin:5px; border-radius: 3px;"
                                oninput="this.nextElementSibling.value = this.value">
                                 <output>{element.get('value', '')}</output>'''
         case "select" | "radio" | "checkbox":
@@ -55,23 +58,30 @@ def coinput(**elements):
                 for option in element.get("options",[])
             )
             if element["type"] == "select":
-                return f'''<label for="{element['id']}">{element["label"]}</label>
-                            <select id="{element['id']}" name="{element['name']}">\n{optionsHTML}\n</select>'''
-            return f'''<div {'style="display: flex;"' if element['label'][-4:]!='<br>' else ""}><label for="{element['id']}">{element["label"]}</label>\n<div>{optionsHTML}</div></div>'''
+                return f'''<label for="{element['id']}" style="margin-top:5px;" >{element["label"]}</label>
+                            <select id="{element['id']}" name="{element['name']}" 
+                               style="margin-top:5px; margin-bottom:5px; border-radius: 3px;">\n{optionsHTML}\n</select>'''
+            return f'''<div {'style="display: flex; margin-top:5px; margin-bottom:5px; border-radius: 3px;"' if element['label'][-4:]!='<br>' else ""}><label for="{element['id']}">{element["label"]}</label>\n<div>{optionsHTML}</div></div>'''
         case "textarea":
-            return f'''<label for="{element['id']}" style="vertical-align: top;">{element["label"]}</label>
+            return f'''<div style=" margin-top:5px; margin-bottom:5px;">
+            <label for="{element['id']}" style="vertical-align: top;">{element["label"]}</label>
                         <textarea id="{element['id']}" 
                                   name="{element['name']}" 
-                                  style="background-color: #383838; color: #ccc; opacity: 1; border-color: #ccc;" value="{element.get('value', '')}"  placeholder="{element.get('placeholder','')}" 
-                                    >{element.get('value', '')}</textarea>'''
+                                  style="background-color: #383838; color: #ccc; opacity: 1; border-color: #ccc; padding:3px; margin-left:5px; border-radius: 3px;" value="{element.get('value', '')}"  placeholder="{element.get('placeholder','')}" 
+                                    >{element.get('value', '')}</textarea></div>'''
         case _:
             return "Unknown element type"
-    buttontext = "Enter ↩️"
+    buttontext = "Enter ⮨"
     for element in elements.values():
         display(HTML(getHTML(element)))
-        buttontext = element.get("buttontext", buttontext)
-    btnstyle = '"background-color: #444; color: #ccc; opacity: 1; border: 1px solid #444;"'
-    display(HTML(f'''<button id="EnterBtn" style = {btnstyle}>{buttontext}</button>\n'''))
+        buttontext = element.get("buttontext", "  "+buttontext+"  ")
+    btnstyle = '"background-color: #383838; color: #8ab5f8; opacity: 1; border: 1px solid #616161; padding-top:4px; padding-bottom:4px; padding-left:14px; padding-right:14px; margin:2px; border-radius: 4px;"'
+    display(HTML(f'''<button id="EnterBtn" style="background-color: #383838; color: #8ab5f8; opacity: 1; border: 1px solid #616161; padding: 4px 14px; margin: 2px; border-radius: 4px; cursor: pointer; transition: background-color 0.3s, border-color 0.3s; font-weight:bold; overflow: hidden;"
+        onmouseover="this.style.backgroundColor='#383848'"
+        onmouseout="this.style.backgroundColor='#383838'"
+        onmousedown="this.style.borderColor='#fff';this.style.backgroundColor='#484858';"
+        onmouseup="this.style.borderColor='#616161'"
+        onblur="this.style.borderColor='#616161'">{buttontext}</button>\n'''))
 
     inputValuesJS = ""
     for element in elements.values():
@@ -163,6 +173,7 @@ new Promise((resolve) => {
       var inputValues = [];
       var style="background-color: #383838; color: #ccc; opacity: 1; border-color: #383838;"
       '''+inputValuesJS+'''
+      //alert(inputValues);
       resolve(inputValues.length === 1 ? inputValues[0] : inputValues);
     }
   });
@@ -171,7 +182,6 @@ new Promise((resolve) => {
     
 def _coinputsingle(**element):
     name = "input_"+_varnamefriendly(str(element))[:50]+"_"+_varnamefriendly(str(time.time()))
-    #print(name)
     return coinput(**{name:element})
 
 def inputradio(label="Pick an Option: ", options=["Option A", "Option B", "Option C"], **kwargs):
